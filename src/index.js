72580633f74ed3f2b804784041ffdddce4836eb3
Takamichi-tsutsumi <takamichi_tsutsumi@waku-2.com>
2017-10-24 05:16:38 +0900

    react native web init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import { AppRegistry, Image, StyleSheet, Text, View } from 'react-native'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Components
+const Card = ({ children }) => <View style={styles.card}>{children}</View>;
+const Title = ({ children }) => <Text style={styles.title}>{children}</Text>;
+const Photo = ({ uri }) => <Image source={{ uri }} style={styles.image} />;
+const App = () => (
+  <Card>
+    <Title>App Card</Title>
+    <Photo uri="http://www.gstatic.com/webp/gallery/1.jpg" />
+  </Card>
+)
+
+// Styles
+const styles = StyleSheet.create({
+  card: {
+    flexGrow: 1,
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: '1.25rem',
+    fontWeight: 'bold'
+  },
+  image: {
+    height: 40,
+    marginVertical: 10,
+    width: 40
+  }
+})
+
+// App registration and rendering
+AppRegistry.registerComponent('MyApp', () => App)
+AppRegistry.runApplication('MyApp', { rootTag: document.getElementById('root') })
+
+// Register ServiceWorker
 registerServiceWorker();
