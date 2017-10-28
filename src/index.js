cf692d873d5c6779a02f389877e9674b577172c7
Takamichi-tsutsumi <takamichi_tsutsumi@waku-2.com>
2017-10-29 05:04:38 +0900

    formik test

--- src/index.js
+++ src/index.js
@@ -1,34 +1,91 @@
-import React from 'react'
-import { AppRegistry, Image, StyleSheet, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { AppRegistry, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { Formik } from 'formik';
 import registerServiceWorker from './registerServiceWorker';
 
-// Components
-const Card = ({ children }) => <View style={styles.card}>{children}</View>;
-const Title = ({ children }) => <Text style={styles.title}>{children}</Text>;
-const Photo = ({ uri }) => <Image source={{ uri }} style={styles.image} />;
-const App = () => (
-  <Card>
-    <Title>App Card</Title>
-    <Photo uri="http://www.gstatic.com/webp/gallery/1.jpg" />
-  </Card>
-)
+const colors = {
+  pink: '#FFAAA5',
+  lime: '#DCEDC1',
+  yellow: '#FDCF58',
+  gray: '#757676',
+  lightgray: '#D5D5D5',
+  black: '3A3B3B'
+};
+
 
 // Styles
 const styles = StyleSheet.create({
-  card: {
-    flexGrow: 1,
-    justifyContent: 'center'
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start'
   },
-  title: {
-    fontSize: '1.25rem',
-    fontWeight: 'bold'
+  textInput: {
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    borderColor: 'lightgray',
+    borderRadius: '4px',
+    margin: '8px',
+    padding: '8px'
   },
-  image: {
-    height: 40,
-    marginVertical: 10,
-    width: 40
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '8px',
+    margin: '8px',
+    borderStyle: 'solid',
+    borderColor: colors.yellow,
+    borderWidth: '2px',
+    borderRadius: '8px',
+  },
+  buttonText: {
+    color: colors.yellow,
+    fontWeight: 'bold',
   }
-})
+});
+
+
+// Components
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { users: [] };
+  }
+
+  render() {
+    return <View style={styles.container}>
+      <Formik
+        initialValues={{ name: '', email: '' }}
+        onSubmit={(val) => this.setState({users: [val, ...this.state.users]})}
+        render={props => <View>
+            <TextInput
+              name={'name'}
+              placeholder={'Name'}
+              style={styles.textInput}
+              onChangeText={(text) => props.setFieldValue('name', text)}
+            />
+            <TextInput
+              name={'email'}
+              placeholder={'Email'}
+              style={styles.textInput}
+              onChangeText={(text) => props.setFieldValue('email', text)}
+            />
+            <TouchableOpacity
+              onPress={props.handleSubmit}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>ADD TO LIST</Text>
+            </TouchableOpacity>
+          </View>
+        }
+      />
+      <View>{
+        this.state.users.map(user => {
+          return <View key={user.name}><Text>{`${user.name} ${user.email}`}</Text></View>
+        })
+      }</View>
+    </View>
+  }
+}
 
 // App registration and rendering
 AppRegistry.registerComponent('MyApp', () => App)
