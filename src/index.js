1bc99442b48a8cbc1597a0e1e829d54a5097f8e8
Takamichi-tsutsumi <takamichi_tsutsumi@waku-2.com>
2017-10-29 16:32:12 +0900

    add validation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import { AppRegistry, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
 import { Formik } from 'formik';
+import yup from 'yup';
 import registerServiceWorker from './registerServiceWorker';
 
 const colors = {
   pink: '#FFAAA5',
-  lime: '#DCEDC1',
-  yellow: '#FDCF58',
+  lime: '#A5FFB9',
+  yellow: '#FFD1A3',
+  purple: '#A3A3FF',
   gray: '#757676',
   lightgray: '#D5D5D5',
-  black: '3A3B3B'
+  black: '#3A3B3B',
+  error: '#FFA3A3',
+  success: '#A3FFA3',
 };
 
 
@@ -22,10 +26,22 @@ const styles = StyleSheet.create({
   textInput: {
     borderStyle: 'solid',
     borderWidth: '1px',
-    borderColor: 'lightgray',
+    borderColor: colors.lightgray,
     borderRadius: '4px',
     margin: '8px',
     padding: '8px'
+  },
+  inputError: {
+    borderColor: colors.error,
+  },
+  inputSuccess: {
+    borderColor: colors.success,
+  },
+  inputErrorText: {
+    color: colors.error,
+    fontSize: 10,
+    paddingHorizontal: '10px',
+    marginBottom: '8px',
   },
   button: {
     justifyContent: 'center',
@@ -43,6 +59,12 @@ const styles = StyleSheet.create({
   }
 });
 
+// validation schema for form client validation
+const userSchema = yup.object({
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+});
+
 
 // Components
 class App extends Component {
@@ -55,28 +77,49 @@ class App extends Component {
     return <View style={styles.container}>
       <Formik
         initialValues={{ name: '', email: '' }}
-        onSubmit={(val) => this.setState({users: [val, ...this.state.users]})}
-        render={props => <View>
+        onSubmit={(val) => {
+          this.setState({users: [val, ...this.state.users]});
+          console.log(val);
+        }}
+        validationSchema={userSchema}
+    render={({ handleSubmit, handleBlur, setFieldValue, touched, errors, values }) => {
+      console.log(touched)
+      console.log(errors)
+
+          return <View>
             <TextInput
               name={'name'}
               placeholder={'Name'}
-              style={styles.textInput}
-              onChangeText={(text) => props.setFieldValue('name', text)}
+              style={[styles.textInput,
+                touched.name && errors.name ?
+                  styles.inputError :
+                  touched.name && styles.inputSuccess
+              ]}
+              onChangeText={(text) => setFieldValue('name', text)}
+              onBlur={handleBlur}
             />
+            { touched.name && errors.name && <Text style={styles.inputErrorText}>{ errors.name }</Text> }
             <TextInput
               name={'email'}
               placeholder={'Email'}
-              style={styles.textInput}
-              onChangeText={(text) => props.setFieldValue('email', text)}
+              style={[styles.textInput,
+                touched.email && errors.email ?
+                  styles.inputError :
+                  touched.email && styles.inputSuccess
+              ]}
+              onChangeText={(text) => setFieldValue('email', text)}
+              onBlur={handleBlur}
             />
+            { touched.email && errors.email && <Text style={styles.inputErrorText}>{ errors.email }</Text> }
             <TouchableOpacity
-              onPress={props.handleSubmit}
+              onPress={handleSubmit}
               style={styles.button}
             >
               <Text style={styles.buttonText}>ADD TO LIST</Text>
             </TouchableOpacity>
           </View>
         }
+    }
       />
       <View>{
         this.state.users.map(user => {
